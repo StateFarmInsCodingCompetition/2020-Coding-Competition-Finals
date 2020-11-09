ad9e4f92e85a7ae44c50768774bbb63de69e6ee4
Harshal Sheth <hsheth2@gmail.com>
2020-11-09 12:22:55 -0500

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+
+import 'ag-grid-enterprise';
+import "@ag-grid-enterprise/all-modules/dist/styles/ag-grid.css";
+import "@ag-grid-enterprise/all-modules/dist/styles/ag-theme-alpine.css";
+
+function App() {
+  return (
+    <div className="App">
+      hello there
+    </div>
+  );
+}
+
+export default App;
