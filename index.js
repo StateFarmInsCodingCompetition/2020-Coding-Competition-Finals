e71982d18c3cb1d09ead64481871d863dfb3609c
Alex Kim <alexkimt@gmail.com>
2020-11-09 12:08:47 -0500

    cars mpg chart

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var adultsData;
-var autoMpgData;
+var autosMpgData;
 var autosData;
 var carsData;
 var forestFiresData;
@@ -10,8 +10,8 @@ $.get('./data/adults.csv', function(data) {
   console.log(adultsData)
 });
 
-$.get('/data/auto-mpg.csv', function(data) {
-  autoMpgData = $.csv.toObjects(data);
+$.get('./data/auto-mpg.csv', function(data) {
+  autosMpgData = $.csv.toObjects(data);
 });
 
 $.get('./data/autos.csv', function(data) {
@@ -73,39 +73,34 @@ var myChart = new Chart(ctx, {
 //Load auto mpg chart
 var ctx = document.getElementById('autoMpgChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'line',
+    type: 'scatter',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: [autoMpgData]
         }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+      parsing: {
+        xAxisKey: 'displacement',
+        yAxisKey: 'horsepower'
+      },
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'HorsePower'
+          },
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Displacement'
+          }
+        }]
+      }
     }
 });
 
